ac9e4207a326aa6b644f3191b5e49ae8cf3b2120
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2024-05-14 21:53:28 -0500

    add services soporte

--- src/main/java/com/medkaapp/security/controller/SoporteTecnicoController.java
+++ src/main/java/com/medkaapp/security/controller/SoporteTecnicoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/auth")
 @CrossOrigin
 public class SoporteTecnicoController {
 
