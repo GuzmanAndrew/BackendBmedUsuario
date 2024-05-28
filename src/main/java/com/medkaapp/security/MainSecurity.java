ed6ba99a1efc2f18c54bc77339e72dfe4417fcd0
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2024-05-28 02:27:42 -0500

    add permiteAll RequestMapping

--- src/main/java/com/medkaapp/security/MainSecurity.java
+++ src/main/java/com/medkaapp/security/MainSecurity.java
@@ -57,7 +57,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**", "/api/**"git git).permitAll()
+                .antMatchers("/auth/**", "/api/**", "/medicamentos", "patologias", "/clasificacion-frecuencia").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
